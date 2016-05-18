0e515d330abab16292c9f5af51dafd1fda1fbc64
Chris Gaona <thebeloved88@sbcglobal.net>
2016-05-18 13:26:42 -0700

    Adds startup screen on page load

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-(function() {
+$(function() {
   'use strict';
-})();
+
+  //When the page loads, the startup screen appears.
+  $('body').append('<div class="screen screen-start" id="start"><header><h1>Tic Tac Toe</h1><a href="#" class="button">Start game</a></header></div>');
+
+  
+
+});
